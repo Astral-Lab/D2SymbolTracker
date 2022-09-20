f439eb41bcc99a63d76b49a4e0ee9e8593032174
Astral-Lab <87715775+Astral-Lab@users.noreply.github.com>
2022-09-20 12:12:30 +0100

    Add files via upload

---
+++ js/rune.js
@@ -0,0 +1,48 @@
+const initApp = () => {
+
+    const NEXT_BUTTON = document.querySelector('#next');
+
+    NEXT_BUTTON.addEventListener('click', () => {
+
+        if(JSON.parse(localStorage.getItem('links'))[localStorage.getItem('currentNode')].nextNode === null) {
+
+            window.open('accepted.html', '_self');
+
+        } else {
+
+            window.open(JSON.parse(localStorage.getItem('links'))[localStorage.getItem('currentNode')].nextNode, "_self")
+
+            localStorage.setItem('currentNode', Number(localStorage.getItem('currentNode')) + 1) // Point currentNode to nextNode
+
+        }
+       
+    });
+
+    if(localStorage.getItem('currentNode') !== localStorage.getItem('startNode')) { // Are we at the startNode? A back button should NOT be inserted then
+
+        const BACK_BUTTON = document.createElement('div');
+
+        BACK_BUTTON.setAttribute("id", "back");
+
+        BACK_BUTTON.style.cursor = "grab";  // make back button behaviour consistent with next button
+
+        BACK_BUTTON.appendChild(document.createTextNode("Back"));
+
+        BACK_BUTTON.style.margin = "0 2.5rem 0 2.5rem"; // add space between back and next buttons
+
+        NEXT_BUTTON.style.margin = "0 2.5rem 0 2.5rem"; // add space between back and next buttons
+
+        document.querySelector('#navigation').insertBefore(BACK_BUTTON, NEXT_BUTTON);
+
+        BACK_BUTTON.addEventListener('click', () => {
+
+            window.open(JSON.parse(localStorage.getItem('links'))[localStorage.getItem('currentNode')].previousNode, "_self");  // open next window in sequence
+
+            localStorage.setItem('currentNode', Number(localStorage.getItem('currentNode')) - 1) // Point currentNode to nextNode
+            
+        });
+    }
+
+}
+
+document.addEventListener('DOMContentLoaded', initApp);
